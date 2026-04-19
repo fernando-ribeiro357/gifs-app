29b480c05806887acfd910b12ca1e4587b8ff07c
Fernando da Costa Ribeiro <fernando.ribeiro@uerj.br>
2026-04-18 22:45:33 -0300

    Aumentando a lista de gifs

--- src/app.py
+++ src/app.py
@@ -17,6 +17,8 @@ images = [
     "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3Njg4c293MjNrOHJ3d3BtYzZ4cGk5cDBoaG15emVhYnM3MTh6OHlmcCZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/IJOu30pvxcR4A/giphy.gif",
     "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHU3c3FjMzV0ZGcxc2M4aHllaTFqNmNnb3Vlc3Q0cjNnZHUyejFlMSZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/12gDWD2kgBTFnO/giphy.gif",
     "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MTc4aWJvajZzenprc2xjOWx0ZTlkdzZkbzl1bGRqMmJmbXA2ZjcxOCZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/3oKIPkHVR1luJjgAyA/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MXZlYTJsbWtjZnEwMnZ0cmk0dTkxaXh5eXFhOXN5eHJlYm9vNHExcCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/L31YRVxfExs0U/giphy.gif",
+    "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3OHh1d2M4N2docG5iYjI3cTQ2MDlkZXNuZWp1Z2djOXJlZmxtYzE5eCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/55gGTev7s29mMr1ZNk/giphy.gif",
     "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MTc4aWJvajZzenprc2xjOWx0ZTlkdzZkbzl1bGRqMmJmbXA2ZjcxOCZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/G1SuUoS0Fgskpx1jHZ/giphy.gif",
     "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3M2JwYmxoOTU5Y295N3J6OHYxbjlkOHJ3c2Z4N3VmdDBtbWk0NmhocCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/hL49uM9RwRpekdr52M/giphy.gif",
     "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3NmY3MW51eGZzbzA4djR2MmxleXg5aWtzcm5mOHZvcGxqYzRnc3ZwbyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/H1zIfgQFWJR82Et8Uw/giphy.gif"
